5491a04f41b85faad07467c868245b558fbf28bb
Vladislav Kostyrev <vkostyrev@meta-devs.io>
2022-02-13 22:05:50 +0500

    Сделал рефакторинг кода.

--- src/com/metadevs/PaladinsTasks/PaladinsTasksLesson3/Task3_3.java
+++ src/com/metadevs/PaladinsTasks/PaladinsTasksLesson3/Task3_3.java
@@ -2,25 +2,18 @@ package com.metadevs.PaladinsTasks.PaladinsTasksLesson3;
 
 public class Task3_3 {
     public static void main(String[] args) {
-        int [] numbers = {5, 0, -3, 2};
+        int[] numbers = {5, 0, -3, 2};
 
         for (int i = 0; i < numbers.length; i++) {
             int a = numbers[i];
-            if (a == 0 || a == 2) {
-                a = a + 7;
-            } else {
-                a = a / 10;
-            }
+            a = (a == 0 || a == 2) ? (a = a + 7) : (a = a / 10);
             System.out.println(a);
         }
 
         for (int i = 0; i < numbers.length; i++) {
             int a = numbers[i];
-            if (0 < a && a < 5) {
-                System.out.println("Верно");
-            } else {
-                System.out.println("Неверно");
-            }
+            String s = (0 < a && a < 5) ? (s = "Верно") : (s = "Не верно");
+            System.out.println(s);
         }
     }
 }
